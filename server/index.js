2f787c7fc598dfce714d6fab48882af6229e6c79
Krima222 <manackaakristina@gmail.com>
2022-03-16 02:12:58 +0300

    server: primitive authentication of the user from the server side

--- server/index.js
+++ server/index.js
@@ -24,14 +24,23 @@ function counters() {
 wss.on('connection', function connection(ws) {
     counters(); 
     broadcastMessage()
-    console.log(counter.count);
     ws.send(JSON.stringify(messages))
     // ws.on('close', function close(ws) {
         
     // })
     ws.on('message', function (message) {
-        console.log(JSON.parse(message))
-        ws.send(JSON.stringify(JSON.parse(message)))
+        const newMessage = JSON.parse(message)
+        if (newMessage.title == "authentication") {
+            ws.id = newMessage.id;
+            console.log(ws.id);
+        } else if (newMessage.title == "data-from-app") {
+            uotputMessageClient(message);
+        } else {
+            console.log(JSON.parse(message))
+            // ws.send(JSON.stringify(JSON.parse(message)))
+            outputJastClient(message);
+        }
+         
     })
 })
 
@@ -41,3 +50,18 @@ function broadcastMessage() {
     })
 }
 
+function uotputMessageClient(message) {
+    wss.clients.forEach(client => {
+        if (client.id !== 'app') {
+            client.send(JSON.stringify(JSON.parse(message)))
+        }
+    })
+}
+
+function outputJastClient(message) {
+    wss.clients.forEach(client => {
+        if (client.id === 'app') {
+            client.send(JSON.stringify(JSON.parse(message)))
+        }
+    })
+}
