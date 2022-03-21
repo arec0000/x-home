f69b8301e84f5043386f1ae8b6410b1ad835023e
Nikita <s2015836@x.donstu.ru>
2022-03-21 15:49:32 +0300

    client: init mvp

--- client/src/components/connected-count/connected-count.js
+++
@@ -1,7 +0,0 @@
-import './connected-count.css';
-
-function ConnectedCount({connectedCount}) {
-    return <output className="connected-count">подключено: {connectedCount}</output>
-}
-
-export default ConnectedCount;
