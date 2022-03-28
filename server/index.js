9b836aa52ec07231395166138b999da66c5e1747
Krima222 <manackaakristina@gmail.com>
2022-03-28 14:20:34 +0300

    server: fix problem with the wrong format

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const wss = new ws.Server({
 
 const counter = {
 	title: 'connected-count',
-	count: 0  
+	count: 0
 }
 
 wss.on('connection', (ws) => {
@@ -15,16 +15,30 @@ wss.on('connection', (ws) => {
     sendToApp(counter)
     ws.send(JSON.stringify('Подключение установлено'))
     ws.on('message', function (message) {
-        const newMessage = JSON.parse(message)
-        if (newMessage.title == 'authentication') {
-            ws.id = newMessage.id;
-            ws.send(JSON.stringify(counter))
-        } else if (newMessage.title == 'data-from-app') {
-            sendExceptApp(message);
-            console.log(JSON.parse(message))
-        } else {
-            sendToApp(JSON.parse(message));
-            console.log(JSON.parse(message))
+        try {
+            if (/^[\],:{}\s]*$/.test(message.toString()
+                .replace(/\\["\\\/bfnrtu]/g, '@')
+                .replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
+                .replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+                const newMessage = JSON.parse(message)
+                if (newMessage.title == 'authentication') {
+                    ws.id = newMessage.id;
+                    ws.send(JSON.stringify(counter))
+                } else if (newMessage.title == 'data-from-app') {
+                    sendExceptApp(message);
+                    console.log(JSON.parse(message))
+                } else {
+                    sendToApp(JSON.parse(message));
+                    console.log(JSON.parse(message))
+                }
+            } else {
+                console.log(`Получены данные не в JSON формате ${message.toString()}`)
+                ws.send(JSON.stringify({title: 'error', message: 'Никита просит перевести формат в JSON'}))
+            }
+
+        } catch(error) {
+            console.log(`Ошибка при получении данных, Никита плохой ${error.message}`);
+            ws.send(JSON.stringify({title: 'error', message: `Ошибка при получении данных, Никита плохой ${error.message}`}))
         }
     })
     ws.on('close', () => {
