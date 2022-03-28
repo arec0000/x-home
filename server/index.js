059421c159f514a4e8f61267ee3e61f98315dc46
Krima222 <manackaakristina@gmail.com>
2022-03-28 19:26:07 +0300

    server: code refactoring

--- server/index.js
+++ server/index.js
@@ -19,98 +19,109 @@ const express = require('express');
 // startApp();
 
 
-const app = express();
+const app = express()
 
 const PORT = 3000;
 
 app.listen(PORT, (error) => {
-    error ? console.log(error) : console.log(`Сервер запущен на ${PORT} порту`);
+    error ? console.log(error) : console.log(`Сервер запущен на ${PORT} порту`)
 })
 
-app.use(express.static('build'));
+app.use(express.static('build'))
 //отслеживание гет запросов по этому роуту
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/build/index.html');
+    res.sendFile(__dirname + '/build/index.html')
 });
 
+const websocketServer = {
+    start() {
+        //создаём сервер из пакета ws
+        this.chanel = new ws.Server({port: 5000}, () => console.log('Сервер запущен на 5000'))
 
+        this.chanel.on('connection', (ws) => {
+            console.log('Пользователь подключился')
+            console.log(`Всего пользователей ${this.chanel.clients.size}`)
+            ws.isAlive = true;
+            ws.send(JSON.stringify('Подключение установлено'))
+            ws.on('message', function (message) {
+                try {
+                    if (/^[\],:{}\s]*$/.test(message.toString()
+                        .replace(/\\["\\\/bfnrtu]/g, '@')
+                        .replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
+                        .replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+                        const newMessage = JSON.parse(message)
+                        if (newMessage.title == 'authentication') {
+                            ws.id = newMessage.id;
+                        } else if (newMessage.title == 'data-from-app') {
+                            this.sendExceptApp(newMessage)
+                            console.log(newMessage)
+                        } else {
+                            this.sendToApp(newMessage)
+                            console.log(newMessage)
+                        }
+                    } else {
+                        console.log(`Получены данные не в JSON формате ${message.toString()}`)
+                        ws.send(JSON.stringify({title: 'error', message: 'Никита просит перевести формат в JSON'}))
+                    }
 
-//создаём сервер из пакета ws
-const wss = new ws.Server({
-    port: 5000,
-}, () => console.log('server start'))
-
-const counter = {
-	title: 'connected-count',
-	count: 0
-}
-
-wss.on('connection', (ws) => {
-    console.log('Пользователь подключился')
-    console.log(`Всего пользователей ${wss.clients.size}`)
-    ws.isAlive = true;
-    counter.count = wss.clients.size
-    sendToApp(counter)
-    ws.send(JSON.stringify('Подключение установлено'))
-    ws.on('message', function (message) {
-        try {
-            if (/^[\],:{}\s]*$/.test(message.toString()
-                .replace(/\\["\\\/bfnrtu]/g, '@')
-                .replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
-                .replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
-                const newMessage = JSON.parse(message)
-                if (newMessage.title == 'authentication') {
-                    ws.id = newMessage.id;
-                    ws.send(JSON.stringify(counter))
-                } else if (newMessage.title == 'data-from-app') {
-                    sendExceptApp(message);
-                    console.log(JSON.parse(message))
-                } else {
-                    sendToApp(JSON.parse(message));
-                    console.log(JSON.parse(message))
+                } catch(error) {
+                    console.log(`Ошибка при получении данных, Никита плохой ${error.message}`)
+                    ws.send(JSON.stringify({title: 'error', message: `Ошибка при получении данных, Никита плохой ${error.message}`}))
                 }
-            } else {
-                console.log(`Получены данные не в JSON формате ${message.toString()}`)
-                ws.send(JSON.stringify({title: 'error', message: 'Никита просит перевести формат в JSON'}))
+            })
+
+            ws.on('pong', () =>  ws.isAlive = true)
+
+            ws.on('close', () => {
+                console.log('Пользователь отключился')
+            })
+
+            ws.on('error', () => {
+                console.log(`Ошибка ${error.message}`)
+            })
+        })
+
+        const closeFallenConections = setInterval(() => {
+            this.chanel.clients.forEach(client => {
+                if (!client.isAlive) {
+                    client.terminate();
+                    console.log('Связь с пользователем разорвана');
+                } else {
+                    client.isAlive = false;
+                    client.ping();
+                }
+            })
+        }, 1000)
+
+        this.chanel.on('close', () => {
+            clearInterval(closeFallenConections)
+            console.log('Сервер отключен')
+        })
+
+        this.chanel.on('error', () => {
+            console.log(`Ошибка ${error.message}`)
+        })
+    },
+
+    sendExceptApp(message) {
+        this.chanel.clients.forEach(client => {
+            if (client.id !== 'app') {
+                client.send(JSON.stringify(message))
             }
+        })
+    },
 
-        } catch(error) {
-            console.log(`Ошибка при получении данных, Никита плохой ${error.message}`);
-            ws.send(JSON.stringify({title: 'error', message: `Ошибка при получении данных, Никита плохой ${error.message}`}))
-        }
-    })
-    ws.on('pong', () =>  ws.isAlive = true)
-    ws.on('close', () => {
-        counter.count = wss.clients.size
-        sendToApp(counter)
-        console.log('Пользователь отключился')
-    })
-})
-
-function sendExceptApp(message) {
-    wss.clients.forEach(client => {
-        if (client.id !== 'app') {
-            client.send(JSON.stringify(JSON.parse(message)))
-        }
-    })
+    sendToApp(message) {
+        this.chanel.clients.forEach(client => {
+            if (client.id === 'app') {
+                client.send(JSON.stringify(message))
+            }
+        })
+    }
 }
 
-function sendToApp(message) {
-    wss.clients.forEach(client => {
-        if (client.id === 'app') {
-            client.send(JSON.stringify(message))
-        }
-    })
+try {
+    websocketServer.start()
+}   catch(error) {
+    console.log(`Ошибка при запуске сервера ${error.message}`)
 }
-
-const closeFallenConections = setInterval(() => {
-    wss.clients.forEach(client => {
-        if (!client.isAlive) {
-            client.terminate();
-            console.log('Связь с пользователем разорвана');
-        } else {
-            client.isAlive = false;
-            client.ping();
-        }
-    })
-}, 1000)
