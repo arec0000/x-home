d3c1ad7daed026b46ac7ea21d1bf0045aceb17d9
Nikita <s2015836@x.donstu.ru>
2022-03-28 13:14:53 +0300

    client: add favicon and refactor socket module

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -59,7 +59,7 @@ class App extends Component {
     }
 
     componentDidMount() {
-        socket.connect(this.onMessage);
+        socket.connect('ws://localhost:5000', this.onMessage);
     }
 
     render() {
