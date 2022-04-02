43c2c1fb804f7a6bec42d1e1e9e63d0cbcc2b15c
Krima222 <manackaakristina@gmail.com>
2022-04-03 00:49:44 +0300

    client: add data exchange with the internal farm server

--- client/src/components/greenhouse-Inside/greenhouse-inside.js
+++ client/src/components/greenhouse-Inside/greenhouse-inside.js
@@ -13,17 +13,52 @@ class GreenhouseInside extends Component {
                 ],
                 dataetime: '',
                 control: [
-                    {id: 'pump_state', name: 'Полив', state: true},
-                    {id: 'led_state', name: 'Свет', state: false},
-                    {id: 'water_level', name: 'Уровень воды', state: false}
+                    {id: 'pump_state', name: 'Полив', toggleRoute: '/pump/config', state: false},
+                    {id: 'led_state', name: 'Свет', toggleRoute: '/led/config', state: false},
+                    {id: 'water_level', name: 'Уровень воды',state: false}
                 ]
         }
     }
 
-    onToggleFarmControl = (id) => {
-        this.setState(({control}) => ({
-            control: control.map(item => item.id === id ? {...item, state: !item.state} : item)
-        }))
+    onToggleFarmControl = (id, toggleRoute) => {
+        if (toggleRoute) {
+            this.setState(({control}) => ({
+                control: control.map(item => item.id === id ? {...item, state: !item.state} : item)
+            }))
+            fetch(`http://predeludel.pythonanywhere.com${toggleRoute}`)
+                .catch(error => console.error(`Ошибка при отправке данных на http://predeludel.pythonanywhere.com${toggleRoute} ${error}`))
+        }
+    }
+
+    sendRequest = async (link) => {
+        const response = await fetch(link)
+        const {data} = await response.json()
+        if (this._isMounted) {
+            new Promise(resolve =>
+                this.setState({
+                    dataetime: data.dataetime,
+                    sensors: [
+                        {id: 'water_temp_c', name: 'Температура воды', value: data.water_temp_c, measure: '°'},
+                        {id: 'air_temp_c', name: 'Температура воздуха', value: data.air_temp_c, measure: '%'},
+                        {id: 'humidity', name: 'Влажность', value: data.humidity, measure: '%'}
+                    ],
+                    control: [
+                        {id: 'pump_state', name: 'Полив', toggleRoute: '/pump/config', state: data.pump_state},
+                        {id: 'led_state', name: 'Свет', toggleRoute: '/led/config', state: data.led_state},
+                        {id: 'water_level', name: 'Уровень воды', state: data.water_level}
+                    ]
+                }, resolve)
+            ).then(() => setTimeout(() => this.sendRequest(link), 3000))
+        }
+    }
+
+    componentDidMount = () => {
+        this._isMounted = true
+        this.sendRequest('http://predeludel.pythonanywhere.com/api/data')
+    }
+
+    componentWillUnmount = () => {
+        this._isMounted = false
     }
 
     render() {
@@ -39,7 +74,7 @@ class GreenhouseInside extends Component {
                 <button
                     key={item.id}
                     className= {item.state ? "control__button active" : "control__button"}
-                    onClick={() => this.onToggleFarmControl(item.id)}>
+                    onClick={() => this.onToggleFarmControl(item.id, item.toggleRoute)}>
                     {item.name}
                 </button>
             )
