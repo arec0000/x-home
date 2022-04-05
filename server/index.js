8c4f9c6cef4365e9eb19bdda1293f555ee354b56
Nikita <s2015836@x.donstu.ru>
2022-04-05 12:06:15 +0300

    server add routing for app

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import websocketServer from './websocketServer.js'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 mongoose.connect('mongodb://81.163.29.85:27017/x-home', {useNewUrlParser: true, useUnifiedTopology: true})
 
 const fast = express()
+const _dir = dirname(fileURLToPath(import.meta.url))
 
 fast.listen(3000, (error) => {
     error ? console.log(error) : console.log(`Сервер запущен на 3000 порту`)
@@ -13,8 +16,20 @@ fast.listen(3000, (error) => {
 fast.use(express.static('build'))
 //отслеживание гет запросов по этому роуту
 fast.get('/', (req, res) => {
-    res.sendFile(__dirname + '/build/index.html')
-});
+    res.sendFile(_dir + '/build/index.html')
+})
+
+fast.get('/greenhouse-outside', (req, res) => {
+    res.sendFile(_dir + '/build/index.html')
+})
+
+fast.get('/greenhouse-inside', (req, res) => {
+    res.sendFile(_dir + '/build/index.html')
+})
+
+fast.get('/robot-control', (req, res) => {
+    res.sendFile(_dir + '/build/index.html')
+})
 
 try {
     websocketServer.start()
