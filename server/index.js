07012b6f3e67af83f8cf04b810c7bc91d9d80e65
Nikita <s2015836@x.donstu.ru>
2022-04-06 23:04:22 +0300

    Add landing page by Andrey Fomenko

--- server/index.js
+++ server/index.js
@@ -4,16 +4,17 @@ import websocketServer from './websocketServer.js'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
-mongoose.connect('mongodb://81.163.29.85:27017/x-home', {useNewUrlParser: true, useUnifiedTopology: true})
-
 const fast = express()
 const _dir = dirname(fileURLToPath(import.meta.url))
+
+mongoose.connect('mongodb://81.163.29.85:27017/x-home', {useNewUrlParser: true, useUnifiedTopology: true})
 
 fast.listen(3000, (error) => {
     error ? console.log(error) : console.log(`Сервер запущен на 3000 порту`)
 })
 
 fast.use(express.static('build'))
+fast.use(express.static('landing'))
 //отслеживание гет запросов по этому роуту
 fast.get('/', (req, res) => {
     res.sendFile(_dir + '/build/index.html')
@@ -29,6 +30,10 @@ fast.get('/greenhouse-inside', (req, res) => {
 
 fast.get('/robot-control', (req, res) => {
     res.sendFile(_dir + '/build/index.html')
+})
+
+fast.get('/about', (req, res) => {
+    res.sendFile(_dir + '/landing/index.html')
 })
 
 try {
